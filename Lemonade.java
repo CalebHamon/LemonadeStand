b0b687db34e65220d11aeca9dc5a205b1dd6a560
Caleb <hamoncal25@7hills.org>
2023-09-18 12:59:24 -0400

    Adding pitcher functionality to code,subtracting given money and adding how much pitchers you bought

--- Lemonade.java
+++ Lemonade.java
@@ -1,9 +1,36 @@
+
+import java.util.Scanner;
+
 public class Lemonade {
     public static void main(String[] args) {
         double givenMon = 20.00;
+        String input;
+        String pitcher = "PITCHER";
+        int inputMath;
+        double mrWorldWide;
+        double yNeg;
+        int manyP;
+        int manyIB;
+        int manyL;
+        int manyCOS;
+        Scanner scan = new Scanner(System.in);
         System.out.println("STORE:"); 
         System.out.println("PITCHER @ $2.00");
-        System.out.println("ICE BAGS (100 CUBES PER PACK):"); 
+        System.out.println("ICE BAGS (100 CUBES PER PACK): $1.05");
+        System.out.println("LEMONS: $.06");
+        System.out.println("CUPS OF SUGAR: $0.29");
+        System.out.println("WHAT WOULD YOU LIKE?");
+        input = scan.nextLine();
+        if(input.toUpperCase() == pitcher) {
+            System.out.println("HOW MUCH WOULD YOU LIKE?");
+            inputMath = scan.nextInt();
+            mrWorldWide = inputMath*2;
+            yNeg = givenMon - mrWorldWide;
+            if(!(yNeg < 0)) {
+                givenMon = yNeg;
+
+            }
+        }
 
     }
 }
