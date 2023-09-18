c3031786871f60604ccb206fc3fff93c973034ea
Caleb <hamoncal25@7hills.org>
2023-09-18 12:41:21 -0400

    Starting to make the ingredients list

---
+++ Lemonade.java
@@ -0,0 +1,9 @@
+public class Lemonade {
+    public static void main(String[] args) {
+        double givenMon = 20.00;
+        System.out.println("STORE:"); 
+        System.out.println("PITCHER @ $2.00");
+        System.out.println("ICE BAGS (100 CUBES PER PACK):"); 
+
+    }
+}
